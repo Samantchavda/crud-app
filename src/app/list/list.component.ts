6dd01ec9430e506c27ad5e2b36f8ba25ce2077ea
Samantchavda <chavdasamant03@gmail.com>
2023-05-19 20:42:12 +0530

    Added pagination
    Added pagination using angular material

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { User } from '../user.model';
 import { UserService } from '../user.service';
 
@@ -7,14 +9,22 @@ import { UserService } from '../user.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit {
 
   users: User[] = [];
   displayedColumns: string[] = ['id', 'name', 'email', 'actions'];
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<User>();
+  
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.users = this.userService.getUsers();
+    this.users = this.userService.getUsers();;
+    this.dataSource = new MatTableDataSource<User>(this.users);
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
