054909c43a44a81cb4967728ba930082a5c3cdd4
Samantchavda <chavdasamant03@gmail.com>
2023-05-20 23:00:10 +0530

    Adding ReactiveFormsModule
    Adding reactive form module for validations

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../user.model';
 import { UserService } from '../user.service';
@@ -13,6 +14,11 @@ export class CrudComponent implements OnInit {
   headerText: string = "test";
   users: User[] = [];
 
+  userForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
   user: User = {
     id: 0,
     name: '',
@@ -26,19 +32,28 @@ export class CrudComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((param) => {
       var id = Number(param.get('id'));
-      if(id > 0){
+      if (id > 0) {
         this.user = this.userService.getUser(id);
+        this.userForm.patchValue({
+          name: this.user.name,
+          email: this.user.email
+        });
       }
     });
   }
 
-  saveUser(user: User): void {
-    if(user.id == 0){
-      this.userService.addUser(user);
-    }else{
-      this.userService.updateUser(user);
-    }
-    this.router.navigate(['/users']);
+  saveUser(): void {
+    if (this.userForm.valid && this.userForm.dirty) {
+      // Then copy over the values from the form
+      // This ensures values not on the form, such as the Id, are retained
+      const user: User = { ...this.user, ...this.userForm.value };
+      if (user.id == 0) {
+        this.userService.addUser(user);
+      } else {
+        this.userService.updateUser(user);
+      }
+      this.router.navigate(['/users']);
+    };
   }
 
   deleteUser(id: number): void {
